32d5ed3b1dec964c5993c3dbce07b639c2a54344
Sourabh U Desai <sourabhdesai192@gmail.com>
2026-01-20 21:01:45 +0530

    Create Day4.PatternProblem.java

---
+++ Day4.PatternProblem.java
@@ -0,0 +1,11 @@
+public class loops{
+  public static void main(string[] args){
+    int n=4;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        system.out.print("*");
+      }
+    }
+    system.out.println();
+  }
+}
